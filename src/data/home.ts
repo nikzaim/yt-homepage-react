59c16a3f9ae94191ac1700595fe19319534736eb
nikza <156214902+nikzaim@users.noreply.github.com>
2024-04-30 23:20:25 +0800

    Add category pills component

---
+++ src/data/home.ts
@@ -0,0 +1,1 @@
+export const categories = ["All", "JavaScript", "TypeScript", "Programming", "Weight Lifting", "Bowling", "Hiking", "React", "Next.js", "Functional Programming", "OOP", "Coding", "Web Dev"];
